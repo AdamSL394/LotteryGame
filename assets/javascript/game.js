9e34338a7b9a3dca491113ae5c6f770fd28338ff
Adam <lehreradam@yahoo.com>
2019-05-24 23:41:31 -0700

    templet set up psuedo code

---
+++ assets/javascript/game.js
@@ -0,0 +1,16 @@
+// Var wins : 
+// var losses : 
+// Var number between 19-120 : randomonly generated at page load an new game
+// Crystals 1 - random number goes in 1-12 and is locked for the game + on click is added to score / at reset game value is changed 
+// Crystals 2 - random number goes in and is locked for the game + on click is added to score / at reset game value is changed 
+// Crystals 3 - random number goes in and is locked for the game + on click is added to score / at reset game value is changed 
+// Crystals 4 - random number goes in and is locked for the game + on click is added to score / at reset game value is changed 
+// Your score  - when equal to the  var random number  win game, when passes var random number loss game
+ 
+var win=[];
+var loss=[];
+var targetNumber=[];
+var tally=[];
+
+// targetNumber=(Math.floor(Math.random()* 119)
+// console.log(targetNumber)
