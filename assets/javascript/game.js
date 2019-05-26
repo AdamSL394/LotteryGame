54885903e5e2a283f872216ff27b1df4a83f7d37
Adam <lehreradam@yahoo.com>
2019-05-25 19:28:17 -0700

    button 1 & 2 generating random number

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,10 +7,49 @@
 // Crystals 4 - random number goes in and is locked for the game + on click is added to score / at reset game value is changed 
 // Your score  - when equal to the  var random number  win game, when passes var random number loss game
  
-var win=[];
-var loss=[];
-var targetNumber=[];
+var win=0;
+var loss=0;
+var box1Number=[];
+var box2Number=[];
+var box3Number=[];
+// var computerChoice4=[]
 var tally=[];
 
-// targetNumber=(Math.floor(Math.random()* 119)
-// console.log(targetNumber)
+$(document).ready(function(){
+
+
+// Button1 when clicked generates a number between 1-12
+$("#one").on("click",function(){
+var boxoneguess = [(Math.floor(Math.random()* 12))+1];
+box1Number.push(boxoneguess);
+$("#score").text(box1Number);
+})
+
+// Button2 when clicked generates a number between 1-12
+$("#two").on("click",function(){
+    var boxtwoguess =[(Math.floor(Math.random()* 12))+1];
+    box2Number.push(boxtwoguess);
+    console.log(boxtwoguess);
+    $("#score").text(boxtwoguess);
+})
+// Button3 when clicked generates a number between 1-12
+$("#three").on("click",function(){
+    var boxthreeguess =[(Math.floor(Math.random()* 12))+1];
+    box3Number.push(boxthreeguess);
+    $("#score").text(boxthreeguess);
+})
+
+//     // Button4 when clicked generates a number between 1-12
+// $("#four").on("click",function(){
+//     var computerGuess =[(Math.floor(Math.random()* 12))+1];
+//     computerChoice4.push(computerGuess);
+// })
+
+
+
+console.log(box1Number); 
+// console.log(boxoneguess); 
+// console.log(boxtwoguess);
+
+})
+
